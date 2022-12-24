97b092724d7e80d632034533d9e6ca70219f06b0
Пчелинцев Дмитрий <S90Dima@yandex.com>
2022-12-24 13:11:08 +0300

    minichages

--- ChessNetwork/Assets/Player/Player.cs
+++ ChessNetwork/Assets/Player/Player.cs
@@ -11,31 +11,34 @@ public class Player : NetworkBehaviour
     private static List<Player> list = new();
     private static UnityEvent<Info> _connected = new();
     private static UnityEvent<Info> _disconected = new();
+    private static Dictionary<ulong, Player> _playerById = new();
 
     public static List<Player> List => new(list);
     public static UnityEvent<Info> Connected => _connected;
     public static UnityEvent<Info> Disconected => _disconected;
     public string Nickname => _nickname.Value.ToString();
-
+    public static Dictionary<ulong, Player> PlayerById => _playerById;
     public Info InstanceInfo => new Info() 
     {
         Nickname = _nickname,
         ClientID = OwnerClientId
     };
 
-    public void Start()
+    public override void OnNetworkSpawn()
     {
         if (IsClient && IsLocalPlayer)
         {
             _nickname.Value = LocalPlayer.Nickname;
         }
         list.Add(this);
+        _playerById.Add(OwnerClientId, this);
         _connected.Invoke(InstanceInfo);
     }
 
     public override void OnDestroy()
     {
         list.Remove(this);
+        _playerById.Remove(OwnerClientId);
         _disconected.Invoke(InstanceInfo);
     }
 
